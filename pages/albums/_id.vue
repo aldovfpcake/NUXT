2150e2ebc3f095273daabe8f05fdc4bac411151c
ALDOVFPCAKE <alvalen@gmail.com>
2021-09-14 12:51:40 -0300

    primer commit

---
+++ pages/albums/_id.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <h1> Hello Word </h1>
+    </div>
+</template>
+<script>
+import router from 'vue-router';
+export default {
+    name: 'AlbunIndvPage',
+    created() {
+        console.log(this.$route);
+    },
+    
+}
+</script>
+<style lang="">
+    
+</style> 
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="">
+      <div clas = "container">
+        <h1 class= "title"> Albums de la página </h1>
+        <div class="columns"
+           <AlbumCard :album="album" v-for="album in albums":key="album.id"/>
+        </div>
+      </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import env from '../config/env';
+import AlbumCard from '../components/AlbumCard';
+export default {
+    name :'IndexPage',
+    components:{
+       AlbumCard:AlbumCard
+    },
+    data(){
+        return{
+            albums:[]
+        }
+    },
+    created(){
+             console.log('${env.endpoint}/todos'),
+            axios.get('https://jsonplaceholder.typicode.com/todos').then(response=>{
+            this.albums = response.data;
+        })
+    }
+     
+}
+</script>
